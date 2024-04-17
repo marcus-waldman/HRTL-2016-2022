28386209e5ce65fa82c7f3f4aa8ecb5eef823b55
marcus-waldman <marcus.waldman@unmc.edu>
2024-04-17 10:56:23 -0600

    groundwork for constructing mplus data file and syntax

--- dinking_around.R
+++ dinking_around.R
@@ -1,4 +1,21 @@
-# 
+rm(list = ls())
+
+library(tidyverse)
+library(haven)
+library(sjlabelled)
+library(stringr)
+library(readxl)
+
+# Change repo directory
+#repo_wd = "C:/cloned-directories/HRTL-2016-2022/HRTL-2016-2022"
+repo_wd = "C:/repos/HRTL-2016-2022"
+setwd(repo_wd)
+
+# Initalize functions
+for(x in list.files("functions/", full.names = T)){cat(paste0("\n",x, " successfully sourced")); source(x)}
+
+
+
 # # Load in the raw datasets
 # raw16 = haven::read_spss(file = "datasets/raw/CAHMI-2016/NSCH2016_Topical_SPSS_CAHM_DRCv2.sav")
 # raw17 = haven::read_spss(file = "datasets/raw/CAHMI-2017/2017 NSCH_Topical_CAHMI_DRCv2.sav")
@@ -21,41 +38,73 @@
 
 raw_datasets = read_rds(file = "datasets/intermediate/raw_datasets.rds")
 
+dprior = "ALF(0,1)"
+estimator = "mlr"
+type = "complex"
+link = "probit"
+algorithm = "integration"
+integration = "gauss(8)"
 
-# Motor Development: 7-COUNTTO
-df_e7_1621 = lapply(2016:2021, function(x){
-  recode_it(rawdat = raw_datasets[[as.character(x)]], 
-            year = x, 
-            lex = "e7_1621", 
-            var_cahmi = "COUNTTO", 
-            reverse=F) 
-})  %>% dplyr::bind_rows()
-
-df_e7_22 =  raw_datasets[["2022"]] %>% 
-  recode_it(rawdat = ., 
-            year = 2022, 
-            lex = "e7_22", 
-            var_cahmi = "COUNTTO_R", 
-            reverse=F) 
-
-df_e7 = df_e7_1621 %>% 
-  dplyr::bind_rows(df_e7_22) %>% 
-  dplyr::mutate(across(everything(), zap_all)) %>% 
+# Construct skeleton of analytic dataset
+dat = lapply(2016:2022, function(x){
+  raw_datasets[[as.character(x)]] %>% 
+    dplyr::select(FIPSST,STRATUM,HHID,FWC,SC_AGE_YEARS) %>% 
+    dplyr::filter(SC_AGE_YEARS == 3 | SC_AGE_YEARS == 4 | SC_AGE_YEARS == 5) %>% 
+    dplyr::rename(SC_AGE = SC_AGE_YEARS) %>% 
+    dplyr::rename_all(tolower) %>% 
+    mutate(across(everything(), zap_all)) %>% 
+    mutate(across(where(is.character), as.numeric))%>% 
+    mutate(across(everything(), zap_all)) %>% 
+    dplyr::mutate(year = x) %>% 
+    dplyr::relocate(year)
+}) %>% 
+  dplyr::bind_rows() %>% 
+  dplyr::mutate(recnum = 1:nrow(.)) %>% 
+  dplyr::relocate(recnum) %>% 
   as.data.frame()
 
-syntax_e7 = list(
-  model= c("!e7: COUNTTO (How high can this child count?) COUNTTO_R (If asked to count objects, how high can this child count correctly?)",
-           " EL by e7_1621* (ae7_1) e7_22* (ae7_2);",
-           " [e7_1621$1] (t1e7_1) [e7_22$1] (t1e7_2);", 
-           " [e7_1621$2] (t2e7_1) [e7_22$1] (t2e7_2);"),
-  priors = c("!e7",
-             " diff(ae7_1,ae7_2)~ALF(0,1)", 
-             " diff(t1e7_1, t2e7_2)~ALF(0,1)"
-  ), 
-  constraint = NULL
+# Create skeleton syntax
+title_list = NULL
+data_list = list(FILE = NULL)
+variable_list = 
+  list(
+    NAMES = NULL,
+    USEV = NULL, 
+    CATEGORICAL = NULL, 
+    IDVARIABLE = "recnum", 
+    STRATIFICATION = c("fipsst","stratum"), 
+    CLUSTER = c("hhid")
+)
+analysis_list = 
+  list(
+    TYPE = type, 
+    ESTIMATOR = estimator,
+    ALGORITHM = algorithm,
+    INTEGRATION = integration, 
+    LINK = link
+)
+model_list = list(MODEL = NULL)
+
+syntax_list = list(
+  TITLE = title_list, 
+  DATA = data_list, 
+  VARIABLE = variable_list, 
+  ANALYSIS = analysis_list,
+  MODEL = model_list, 
+  `MODEL PRIORS` = NULL, 
+  `MODEL CONSTRAINT` = NULL
 )
 
-mplus_list = list(e7 = list(data = df_e7, syntax = syntax_e7))
 
-get_cahmi_values_map(rawdat = raw_datasets[["2022"]],var = "COUNTTO_R", reverse = F,reverse_in_mplus = F, force_value_missing = NA)
+
+
+#### Motor Development: ####
+# 7-COUNTTO
+  e7_list = e7(raw_datasets,dprior)
+  
+  # Update the dataset
+  dat = dat %>% safe_left_join(e7_list$data, by = c("year","hhid"))
+  syntax_list = update_syntax(syntax_list, e7_list$syntax)
+  
+
 
---
+++ functions/e7.R
@@ -0,0 +1,54 @@
+e7<-function(raw_datasets, dprior){ # 7-COUNTTO
+  
+  require(tidyverse)
+  
+  #Recode 2016-2021: COUNTTO
+  df_e7_1621 = lapply(2016:2021, function(x){
+    recode_it(rawdat = raw_datasets[[as.character(x)]], 
+              year = x, 
+              lex = "e7_1621", 
+              var_cahmi = "COUNTTO", 
+              reverse=F) 
+  })  %>% dplyr::bind_rows()
+  
+  #Recode 2022: COUNTTO_R
+  df_e7_22 =  raw_datasets[["2022"]] %>% 
+    recode_it(rawdat = ., 
+              year = 2022, 
+              lex = "e7_22", 
+              var_cahmi = "COUNTTO_R", 
+              reverse=F) 
+  
+  #Bind the recoded item response data
+  df_e7 = df_e7_1621 %>% 
+    dplyr::bind_rows(df_e7_22) %>% 
+    dplyr::mutate(across(everything(), zap_all)) %>% 
+    as.data.frame() %>% 
+    dplyr::rename_all(tolower)
+  
+  #Construct Mplus syntax
+  syntax_e7 = list(
+    TITLE = c("!e7_1621 (COUNTTO): How high can this child count?",
+              "!e7_22 (COUNTTO_R): If asked to count objects, how high can this child count correctly?"),
+    VARIABLE = list(USEV = c("e7_1621", "e7_22"), 
+                    CATEGORICAL = c("e7_1621", "e7_22")
+    ),
+    MODEL= c("!e7: COUNTTO (2016-2021) & COUNTTO_R (2022)",
+             " EL by e7_1621*1 (le7_1) e7_22*1 (le7_2)",
+             " [e7_1621$1*] (t1e7_1) [e7_22$1*] (t1e7_2)", 
+             " [e7_1621$2*] (t2e7_1) [e7_22$1*] (t2e7_2)", 
+             " [e7_1621$3*] (t3e7_1) [e7_22$3*] (t3e7_2)",
+             " [e7_1621$4*] (t4e7_1) [e7_22$4*] (t4e7_2)"
+    ),
+    `MODEL PRIORS` = c("!e7: COUNTTO (2016-2021) & COUNTTO_R (2022)",
+                       paste0(" diff(le7_1,le7_2)~", dprior), 
+                       paste0(" diff(t1e7_1, t1e7_2)~", dprior), 
+                       paste0(" diff(t1e7_1, t1e7_2)~", dprior)
+    )
+  )
+  
+  return(list(data = df_e7, syntax = syntax_e7))
+  
+}
+
+
